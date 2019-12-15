4b093bb9b33adc13335a8e6ad0e5084240774fd2
Nader Zaiter <zaayter99@gmail.com>
2019-12-15 10:44:44 +0100

    Feat: Create project

---
+++ marketplace/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MarketplacePage } from './app.po';
+
+describe('marketplace App', () => {
+  let page: MarketplacePage;
+
+  beforeEach(() => {
+    page = new MarketplacePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
