281a63c584872d1ceda3f9e016b7ba36511be0a5
Nader Zaiter <zaayter99@gmail.com>
2019-12-15 12:00:43 +0100

    Feat: Creamos el componente home

--- marketplace/src/app/components/home/home.module.ts
+++ marketplace/src/app/components/home/home.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
